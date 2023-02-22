9679f2a02b076e59b76fc4770f953754b71df0e6
AKButani <butani.arjun@gmail.com>
2023-02-22 19:24:28 +0100

    Add bye with wave reaction

--- bot.py
+++ bot.py
@@ -18,6 +18,12 @@ async def on_ready():
 async def on_message(ctx):
     await ctx.send('Hello!')
 
+@bot.command(name="bye")
+async def on_message(ctx):
+    await ctx.send("Bye! :(")
+    emoji = "👋🏼"
+    await ctx.message.add_reaction(emoji)
+
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(1073167438042120204)
