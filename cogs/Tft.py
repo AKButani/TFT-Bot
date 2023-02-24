7efea6276c16f0db8b2c646afb2e667112bc2633
AKButani <butani.arjun@gmail.com>
2023-02-24 22:12:41 +0100

    /player basic functionality

--- cogs/Tft.py
+++ cogs/Tft.py
@@ -39,10 +39,14 @@ class Tft(commands.Cog):
         region += "1"
         data = watcher.summoner.by_name(region=region, summoner_name=player_name)
         print(data)
-        embed = discord.Embed(title=player_name, )
-        
+        embed = discord.Embed(title=player_name, url = None)
+        embed.set_thumbnail(url="http://ddragon.leagueoflegends.com/cdn/13.4.1/img/profileicon/{}.png".format(data["profileIconId"]))
+        embed.add_field(name="Summoner level:", value=str(data["summonerLevel"]))
+        rank_info = watcher.league.by_summoner(region=region, encrypted_summoner_id=data["id"])[0]
+        print(rank_info)
+        embed.add_field(name="Rank", value=rank_info["tier"] + " " + rank_info["rank"])
         #todo: get image from the id http://ddragon.leagueoflegends.com/cdn/13.4.1/img/profileicon/685.png
-        ctx.send(embed = embed)
+        await ctx.send(embed = embed)
         
         
 
