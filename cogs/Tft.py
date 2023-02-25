c8a1d2f7582d9eca8b28ae312c8e192d0b7640af
AKButani <butani.arjun@gmail.com>
2023-02-25 22:20:06 +0100

    Exception handling

--- cogs/Tft.py
+++ cogs/Tft.py
@@ -1,16 +1,10 @@
 from discord.ext import commands
 import discord
 import config
-from riotwatcher import TftWatcher
+from riotwatcher import TftWatcher, ApiError
 from helpers import tfthelper
 
 watcher = TftWatcher(api_key=config.tft_key)
-
-
-
-
-
-
 
 
 class Tft(commands.Cog):
@@ -23,30 +17,36 @@ class Tft(commands.Cog):
         """ region += "1" #temp sol for popular regions"""
         region = await tfthelper.region_selection(ctx)
 
-        summoner_data = watcher.summoner.by_name(region=region, summoner_name=player_name)
-        #print(summoner_data)
-        rank_info = watcher.league.by_summoner(region=region, encrypted_summoner_id=summoner_data["id"])[0]
-        #print(rank_info)
-
-        #create Embed for message output
         embed = discord.Embed(title=player_name, url = None) 
-        embed.set_thumbnail(url="http://ddragon.leagueoflegends.com/cdn/13.4.1/img/profileicon/{}.png".format(summoner_data["profileIconId"]))
-        embed.add_field(name="Summoner level:", value=str(summoner_data["summonerLevel"]))
-        embed.add_field(name= "Region", value=(region[:-1]).upper())
-        embed.add_field(name="Rank", value=rank_info["tier"] + " " + rank_info["rank"])
-        #add as many fields as necessary
+        try:
+            summoner_data = watcher.summoner.by_name(region=region, summoner_name=player_name)
+            rank_info = watcher.league.by_summoner(region=region, encrypted_summoner_id=summoner_data["id"])[0]
+            
+            embed.set_thumbnail(url="http://ddragon.leagueoflegends.com/cdn/13.4.1/img/profileicon/{}.png".format(summoner_data["profileIconId"]))
+            embed.add_field(name="Summoner level:", value=str(summoner_data["summonerLevel"]))
+            embed.add_field(name= "Region", value=(region[:-1]).upper())
+            embed.add_field(name="Rank", value=rank_info["tier"] + " " + rank_info["rank"])
+            #add as many fields as necessary
+        except ApiError as err:
+            if err.response.status_code == 404:
+                await ctx.send("The player doesn't exist in the given region.")
+                return
+            await ctx.send(err)
+            await ctx.send("Make sure the form is /player <player_name> ")
+            return
+        #print(rank_info)
 
         #TODO exception handling!!
         
         await ctx.send(embed = embed)
         
-
+    """
     @commands.command()
     async def reg(self, ctx):
         chosen = await tfthelper.region_selection(ctx)        
         await ctx.send("Looking for " + "None" + " in " + chosen)
 
-    """
+    
     @commands.command()
     async def timeout_example(self, ctx):
         
