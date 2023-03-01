7edcd371e95c42d60fc5fa9b51a29a457269515c
AKButani <butani.arjun@gmail.com>
2023-03-01 21:57:20 +0100

    Command exception handling

--- cogs/Basic.py
+++ cogs/Basic.py
@@ -28,6 +28,15 @@ class Basic(commands.Cog):
         #channel = bot.get_channel(1073167438042120204)
         await channel.send("Welcome " + member.name)
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("This command doesn't exist. Try using /help to make sure you use the right command")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Your command is missing an argument. Try using /help to make sure you use the right command")
+        else:
+            await ctx.send(error)
+
 
 async def setup(bot):
     await bot.add_cog(Basic(bot))
